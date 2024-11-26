e62a62b6ee25d643020021be59cf1503f15757d5
Arthur D. <arthur@utopios.solutions>
2024-11-26 19:40:09 +0100

    feat: add demo animations (LE RECTANGLE FONCTIONNE!)

---
+++ Demos/Demo06Animations/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo06Animations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            redctangle.RenderTransformOrigin = new Point(0.5, 0.5);
+            redctangle.RenderTransform = new RotateTransform(0);
+        }
+
+        public void Rotate360 (object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation() 
+            { 
+                From = 0, 
+                To = 360, 
+                Duration = new Duration(TimeSpan.FromSeconds(1))
+            };
+
+            var rotateTransform = (RotateTransform)redctangle.RenderTransform;
+
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, doubleAnimation);
+
+        }
+    }
+}
